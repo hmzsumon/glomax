887e7b05f4d387a4fada4d70ac7a883962195067
Zakaria <zaknetsum@gmail.com>
2023-09-09 21:45:22 +0600

    add shair btn

--- src/pages/referral.tsx
+++ src/pages/referral.tsx
@@ -12,7 +12,7 @@ import { BiChevronRight } from 'react-icons/bi';
 import Link from 'next/link';
 import Layout from '@/Layout';
 import ProtectedRoute from '@/global/ProtectedRoute';
-
+import { RWebShare } from 'react-web-share';
 const Referral = () => {
 	const { user } = useSelector((state: any) => state.auth);
 
@@ -30,12 +30,12 @@ const Referral = () => {
 	return (
 		<Layout>
 			<ProtectedRoute>
-				<div className='referral-wrapper py-20'>
+				<div className='py-20 referral-wrapper'>
 					<div className='overlay'></div>
 					<div className='content '>
-						<div className='px-4  md:w-7/12 mx-auto '>
+						<div className='px-4 mx-auto md:w-7/12 '>
 							<div className='grid items-center gap-6 py-2 '>
-								<div className='p-8  w-full bg-black_2 rounded-xl'>
+								<div className='w-full p-8 bg-black_2 rounded-xl'>
 									<h2 className='mb-6 text-xl font-bold '>Default Referral</h2>
 									<div className='space-y-10 '>
 										<div className='flex items-center justify-between p-4 bg-[#12161C] rounded-xl '>
@@ -55,21 +55,23 @@ const Referral = () => {
 										</div>
 
 										<div className='flex items-center grid-cols-8 gap-4 '>
-											<div className='relative flex-1 col-span-7 p-4 bg-yellow-600 rounded-xl'>
-												<div className='absolute hidden md:block bottom-1'>
-													<Image
-														src='/images/referrals/icon.png'
-														width={120}
-														height={50}
-														alt='coin'
-													/>
+											<RWebShare data={{ text: referralLink }}>
+												<div className='relative flex-1 col-span-7 p-4 bg-yellow-600 cursor-pointer rounded-xl'>
+													<div className='absolute hidden md:block bottom-1'>
+														<Image
+															src='/images/referrals/icon.png'
+															width={120}
+															height={50}
+															alt='coin'
+														/>
+													</div>
+													<div>
+														<h2 className='text-2xl font-bold text-center text-gray-800 '>
+															Invite Friend{' '}
+														</h2>
+													</div>
 												</div>
-												<div>
-													<h2 className='text-2xl font-bold text-center text-gray-800 '>
-														Invite Friend{' '}
-													</h2>
-												</div>
-											</div>
+											</RWebShare>
 											<div className='flex items-center justify-center col-span-1 p-4 bg-[#474D57] rounded-xl'>
 												<FaQrcode className='inline-block text-2xl text-gray-400 cursor-pointer ' />
 											</div>
@@ -78,7 +80,7 @@ const Referral = () => {
 								</div>
 							</div>
 						</div>
-						<div className='px-4  space-y-6 '>
+						<div className='px-4 space-y-6 '>
 							<div>
 								<MyReferrals />
 								{/* <MyTask /> */}
