0a85accae1b909cf907f6d03e421d5e6ae10fb8c
Zakaria <zaknetsum@gmail.com>
2023-10-13 11:33:45 +0600

    make it better

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -4,7 +4,8 @@ if (process.env.NODE_ENV === 'development') {
 	baseUrl = 'http://localhost:5005/api/v1';
 	console.log('Running in development mode');
 } else {
-	baseUrl = 'https://wfc-api.herokuapp.com/api/v1';
+	//glomax-api-1901b8e6064a.herokuapp.com/
+	https: baseUrl = 'https://glomax-api-1901b8e6064a.herokuapp.com/api/v1';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
