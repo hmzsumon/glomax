5299c1694a382a460475587208a5dc45333122ab
Zakaria <zaknetsum@gmail.com>
2023-10-29 10:35:13 +0600

    remove vercel.json file

--- src/components/AiRobot/Claim.tsx
+++ src/components/AiRobot/Claim.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { fetchBaseQueryError } from '@/services/helpers';
 import { useClaimAiRobotProfitMutation } from '@/features/aiRobot/aiRobotApi';
@@ -8,6 +8,25 @@ import { ScaleLoader } from 'react-spinners';
 const Claim = () => {
 	const [claimAiRobotProfit, { isLoading, isSuccess, isError, error }] =
 		useClaimAiRobotProfitMutation();
+
+	const [btnFalse, setBtnFalse] = useState<boolean>(false);
+
+	// handle claim
+	const handleClaim = () => {
+		setBtnFalse(true);
+		claimAiRobotProfit(undefined);
+	};
+
+	//after 5 sec set btnFalse to false
+	useEffect(() => {
+		setTimeout(() => {
+			setBtnFalse(false);
+		}, 5000);
+
+		return () => {
+			setBtnFalse(false);
+		};
+	}, [btnFalse]);
 
 	useEffect(() => {
 		if (isError && error) {
@@ -24,8 +43,9 @@ const Claim = () => {
 				<div className='relative p-5 my-5 space-y-3 rounded-md '>
 					<Button
 						color='amber'
-						className='w-full '
-						onClick={() => claimAiRobotProfit(undefined)}
+						className='w-full disabled:opacity-50 disabled:cursor-not-allowed '
+						onClick={handleClaim}
+						disabled={btnFalse || isLoading}
 					>
 						{isLoading ? (
 							<ScaleLoader color='#fff' height={20} width={3} radius={2} />
