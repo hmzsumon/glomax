babec5481c4923391b0407e4de0fdde2d96bb891
Zakaria <zaknetsum@gmail.com>
2023-12-31 06:25:01 +0600

    first commit

--- src/components/Home/Maintenance.tsx
+++ src/components/Home/Maintenance.tsx
@@ -6,21 +6,33 @@ const Maintenance = () => {
 	return (
 		<div className='flex items-center justify-center h-screen '>
 			<div className='text-center'>
-				<h1 className='text-4xl font-bold text-red-500 mb-4'>
-					Under Maintenance
+				<h1 className='mb-4 text-4xl font-bold text-green-500'>
+					Download our new app
 				</h1>
-				<Image
-					src='/maintenance.gif'
-					alt='Maintenance'
-					width={300} // set the width you desire
-					height={200} // set the height you desire
-					className='mb-8 mx-auto'
-				/>
+
+				{/* Download Button */}
+				<div className='my-1 '>
+					<p className='my-1 text-gray-500'>
+						Download our new app from Google Play Store
+					</p>
+					<button>
+						<a
+							href='https://play.google.com/store/apps/details?id=com.wnapp.id1694165800345'
+							target='_blank'
+							rel='noopener noreferrer'
+							className='flex items-center justify-center gap-2 text-yellow-700 '
+						>
+							Download App
+							<FaExternalLinkAlt />
+						</a>
+					</button>
+				</div>
+
 				<p className='text-gray-700'>
 					Sorry for the inconvenience. We are currently undergoing maintenance.
 				</p>
-				<div className=' my-1'>
-					<p className='text-gray-500 my-1'>
+				<div className='my-1 '>
+					<p className='my-1 text-gray-500'>
 						Join our official Telegram group to get all updates of Global X
 					</p>
 					<button>
@@ -28,7 +40,7 @@ const Maintenance = () => {
 							href='https://t.me/+_F020xmmsFAxMWI1'
 							target='_blank'
 							rel='noopener noreferrer'
-							className='text-yellow-700 flex items-center justify-center gap-2 '
+							className='flex items-center justify-center gap-2 text-yellow-700 '
 						>
 							Join our Telegram
 							<FaExternalLinkAlt />
